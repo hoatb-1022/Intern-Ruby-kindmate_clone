d0803434e6fbc4879b823b53fe3c359dd6c98e98
Hoà Trần <tran.ba.hoa@sun-asterisk.com>
2020-07-21 21:14:15 +0700

    Admin features

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -37,4 +37,29 @@ module UsersHelper
       {text: t("users.active"), variant: "success"}
     end
   end
+
+  def user_change_status_link user
+    link_text = t "users.do_active"
+    link_status = 0
+
+    if user.is_blocked?
+      link_text = t "users.do_block"
+      link_status = 1
+    end
+
+    link_to(
+      link_text,
+      admin_user_path(user, status: link_status),
+      method: "patch",
+      class: "dropdown-item"
+    )
+  end
+
+  def user_status_badge user
+    if user.is_blocked?
+      {text: t("users.blocked"), variant: "danger"}
+    else
+      {text: t("users.active"), variant: "success"}
+    end
+  end
 end
