09335922e9eed62f11ba45c8e72fb49a5618c80d
Hoà Trần <tran.ba.hoa@sun-asterisk.com>
2020-07-21 21:14:15 +0700

    Admin features

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,6 +24,11 @@ module ApplicationHelper
     page_title.blank? ? base_title : "#{page_title} | #{base_title}"
   end
 
+  def full_admin_title page_title
+    base_title = t "global.app_admin_title"
+    page_title.blank? ? base_title : "#{page_title} | #{base_title}"
+  end
+
   def get_locale
     I18n.locale
   end
