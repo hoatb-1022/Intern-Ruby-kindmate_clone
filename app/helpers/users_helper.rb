1921d06540f31ae4e29e64b8797e900a8313dacf
Hoà Trần <tran.ba.hoa@sun-asterisk.com>
2020-07-23 13:29:55 +0700

    Custom user avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,38 +6,6 @@ module UsersHelper
     image_tag avatar_url, id: id, class: "gravatar"
   end
 
-  def user_status_options
-    options_for_select(
-      [[t("users.active"), 0], [t("users.blocked"), 1]],
-      params[:status]
-    )
-  end
-
-  def user_change_status_link user
-    link_text = t "users.do_block"
-    link_status = 1
-
-    if user.is_blocked?
-      link_text = t "users.do_active"
-      link_status = 0
-    end
-
-    link_to(
-      link_text,
-      admin_user_path(user, status: link_status),
-      method: "patch",
-      class: "dropdown-item"
-    )
-  end
-
-  def user_status_badge user
-    if user.is_blocked?
-      {text: t("users.blocked"), variant: "danger"}
-    else
-      {text: t("users.active"), variant: "success"}
-    end
-  end
-
   def user_change_status_link user
     link_text = t "users.do_block"
     link_status = 1
